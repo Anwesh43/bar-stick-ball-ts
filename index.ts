239dc3554acea638752bbe098dd0e51002dd0b2e
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 18:41:02 +0530

    adding BSBNode class

--- index.ts
+++ index.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class BSBNode {
+
+    prev : BSBNode 
+    next : BSBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BSBNode {
+        var curr : BSBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
