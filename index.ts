da150e915eb0a4efaa10fde75731d26b67d44a72
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 18:29:47 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -13,6 +13,9 @@ const colors : Array<string> = [
     "#6A1B9A",
     "#00C853"
 ]
+const barWFactor : number = 4.9 
+const barHFactor : number = 9.2 
+const rFactor : number = 24.9 
 
 class ScaleUtil {
 
@@ -22,5 +25,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBarStickBall(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const r : number = Math.min(w, h) / rFactor  
+        const barW : number = Math.min(w, h) / barWFactor 
+        const barH : number = Math.min(w, h) / barHFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + r) * sc3)
+        context.rotate(2 * Math.PI * sc3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawCircle(
+                context,
+                -barW / 3,
+                -h / 2 - r + (h / 2 -barH / 2) * sc2,
+                r
+            )
+            context.restore()
+        }
+        context.fillRect(-(barW / 2) * sc1, -barH / 2, barW, barH)
+        context.restore()
+    }
+
+    static drawBSBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarStickBall(context, scale)
     }
 }
